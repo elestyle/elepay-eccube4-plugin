f50f8e673422aadeda29dbc7d51d821556b8153c
bjj-elestyle <b.jj@elestyle.jp>
2024-03-21 18:53:01 +0900

    perf: release.sh

---
+++ release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+zip -r elepay-eccube4-plugin.zip \
+  Controller \
+  Entity \
+  Form \
+  Repository \
+  Resource \
+  Service \
+  Util \
+  composer.json \
+  Event.php \
+  PluginManager.php
+
